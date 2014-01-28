cfccd5a92e92982bd7cdac572062c4da94a629cf
jrapoport6 <jrapoport@bellsouth.net>
2014-01-27 20:15:38 -0500

    added user, password, and login button on login page

--- src/edu/gatech/cs2340/evergreen/WelcomeActivity.java
+++ src/edu/gatech/cs2340/evergreen/WelcomeActivity.java
@@ -6,18 +6,17 @@ import android.view.Menu;
 
 public class WelcomeActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.welcome, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.welcome, menu);
-        return true;
-    }
-    
 }
