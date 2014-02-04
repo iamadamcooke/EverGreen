3dd6c86bd8fc4c9a9ae99866b248fec5da070e38
Ominous <cmtansey@gatech.edu>
2014-02-03 19:45:38 -0500

    Set up admin login, and incorrect login functionality

--- src/edu/gatech/cs2340/evergreen/LoginActivity.java
+++ src/edu/gatech/cs2340/evergreen/LoginActivity.java
@@ -2,7 +2,14 @@ package edu.gatech.cs2340.evergreen;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
@@ -10,6 +17,75 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		Button loginbtn = (Button) findViewById(R.id.btnLogin);
+		final EditText passwordtxt = (EditText) findViewById(R.id.passwordText);
+		final EditText usernametxt = (EditText) findViewById(R.id.usernameText);
+		final TextView incorrectLogin = (TextView) findViewById(R.id.textView1);
+		
+		incorrectLogin.setVisibility(TextView.INVISIBLE);
+		
+		usernametxt.addTextChangedListener(new TextWatcher(){
+			
+			public void afterTextChanged(Editable s){
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		passwordtxt.addTextChangedListener(new TextWatcher(){
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		
+		loginbtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//switching to register screen
+				if(usernametxt.getEditableText().toString().matches("admin")&&passwordtxt.getText().toString().matches("pass123"))
+				{
+					Intent loginIntent = new Intent(getApplicationContext(), Success.class);
+					startActivity(loginIntent);
+				}else incorrectLogin.setVisibility(TextView.VISIBLE);
+				
+			}
+		});
+			
 	}
 
 	@Override
